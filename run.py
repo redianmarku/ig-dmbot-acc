97bcd633b91884ea7d3e781ba12c84f4fd9a0cbc
Redian Marku <42848358+redianmarku@users.noreply.github.com>
2021-06-05 14:34:41 +0200

    Initial Commit

---
+++ run.py
@@ -0,0 +1,36 @@
+import json
+import random
+from src.instadm import InstaDM
+
+f = open('infos/accounts.json',)
+accounts = json.load(f)
+
+with open('infos/usernames.txt', 'r') as f:
+    usernames = [line.strip() for line in f]
+
+with open('infos/messages.txt', 'r') as f:
+    messages = [line.strip() for line in f]
+
+while True:
+    if not usernames:
+        print('Finished usernames.')
+        break
+
+    for account in accounts:
+        if not usernames:
+            break
+        # Auto login
+        insta = InstaDM(username=account["username"],
+                        password=account["password"], headless=False)
+
+        for i in range(2):
+
+            if not usernames:
+                break
+
+            username = usernames.pop()
+            # Send message
+            insta.sendMessage(
+                user=username, message=random.choice(messages))
+
+        insta.teardown()
